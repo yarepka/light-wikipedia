2d43e794e16f6ce12762ee7b872676a173817bd3
Роман Бубнов <yarepka01@gmail.com>
2020-04-11 21:04:31 +0300

    Initialized application. Set local DB connection

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", (req, res) => {
+    Article.find({}, (err, articles) => {
+        if(err) console.log(err)
+        else console.log(articles);
+    });
+})
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+})
